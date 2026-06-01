ead77c6aaaf4a6fc78183168b2a3bd0cc9b70bef
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-06-02 08:52:39 +1000

    Initial React app setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1>Southern Edge Cricket</h1>
+      <p>Premium cricket bats, gear, and performance equipment.</p>
+      <button>Shop Bats</button>
+    </main>
+  )
+}
+
+export default App;
