f63c86ef72d90645435cb1c273e362b7c4995d9e
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-06-02 11:57:01 +1000

    Updated homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 function App() {
   return (
     <main>
+      <section>
       <h1>Southern Edge Cricket</h1>
       <p>Premium cricket bats, gear, and performance equipment.</p>
       <button>Shop Bats</button>
+      </section>
+
+      <section>
+        <h2>Shop by Category</h2>
+        <ul>
+          <li>Bats</li>
+          <li>Gloves</li>
+          <li>Pads</li>
+          <li>Accesories</li>
+        </ul>
+      </section>
+      
     </main>
   )
 }
