d4c33ff18adf21be931f0ed989486ac6e50fc5f7
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-06-15 09:37:05 +1000

    Added a dropdown filter for brand in the products page.

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,16 +4,37 @@ import { products } from "../data/products";
 
 function ProductsPage({ onAddToCart }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedBrand, setSelectedBrand] = useState("All");
 
-  const filteredProducts = products.filter(
-    (product) =>
+  console.log(selectedBrand);
+
+  const filteredProducts = products.filter((product) => {
+    const matchesSearch =
       product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      product.brand.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+      product.brand.toLowerCase().includes(searchTerm.toLowerCase());
+
+    const matchesBrand =
+      selectedBrand === "All" || product.brand === selectedBrand;
+
+    return matchesSearch && matchesBrand;
+  });
 
   return (
     <section>
       <h1>All products</h1>
+      <select
+        name="Filter Brands"
+        value={selectedBrand}
+        onChange={(event) => setSelectedBrand(event.target.value)}
+      >
+        <option value="All">All Brands</option>
+        <option value="SS">SS</option>
+        <option value="SG">SG</option>
+        <option value="DSC">DSC</option>
+        <option value="GM">GM</option>
+        <option value="Gray Nicolls">Gray Nicolls</option>
+        <option value="Kookaburra">Kookaburra</option>
+      </select>
       <input
         type="text"
         placeholder="Search products..."
