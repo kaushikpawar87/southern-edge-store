a863868910210e34ab85f0c1f460696ea5d0a9b5
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-07-15 10:57:47 +1000

    added the productId route.

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,7 +1,19 @@
-import { getAllProducts } from "../services/productService.js";
+import { getAllProducts, getProductById } from "../services/productService.js";
 
 export function getProducts(req, res) {
   const products = getAllProducts();
 
   res.status(200).json(products);
+}
+
+export function getProduct(req, res) {
+  const productId = Number(req.params.id);
+  const product = getProductById(productId);
+
+  if (!product) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
+  res.status(200).json(product);
 }
--- server/src/services/productService.js
+++ server/src/services/productService.js
@@ -3,3 +3,7 @@ import { products } from "../data/products.js";
 export function getAllProducts() {
   return products;
 }
+
+export function getProductById(productId) {
+  return products.find((product) => product.id === productId);
+}
