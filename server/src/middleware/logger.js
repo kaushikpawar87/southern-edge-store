7c743f58194e25d4f6e863a6e2e99c1ea70b6015
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-08-05 08:41:15 +1000

    added on finish and duration to logger.

--- server/src/middleware/logger.js
+++ server/src/middleware/logger.js
@@ -1,5 +1,11 @@
 export function logger(req, res, next) {
-  console.log(`${req.method} ${req.originalUrl}`);
+  const startTime = Date.now();
+  res.on("finish", () => {
+    const duration = Date.now() - startTime;
+    console.log(
+      `${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`,
+    );
+  });
 
   next();
 }
