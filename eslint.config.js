fbec6213b47eea950a15b440735a34979c8ab2bd
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-08-04 11:24:57 +1000

    configured dotenv and added a .env file.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
+
+  // Frontend React files
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["src/**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -15,7 +17,27 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
   },
-])
+
+  // Backend Node/Express files
+  {
+    files: [
+      "server/**/*.js",
+      "app.js",
+      "server.js",
+      "src/app.js",
+      "src/server.js",
+    ],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      sourceType: "module",
+      globals: globals.node,
+    },
+  },
+]);
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import app from "./app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
