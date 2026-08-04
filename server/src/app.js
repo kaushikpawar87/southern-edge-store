fb96d90c1f3338973326cef36cfc6727343a59ad
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-08-04 11:53:33 +1000

    Added a logger function in middleware.

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import { notFoundHandler, errorHandler } from "./middleware/errorHandler.js";
+import { logger } from "./middleware/logger.js";
 
 const app = express();
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.json({
