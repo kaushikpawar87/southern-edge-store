20fe4cfef33608c71cb0a17bed61cdd7c53654f1
Kaushik Pawar <kaushikpawar87@gmail.com>
2026-08-05 15:58:58 +1000

    added postgresql configuration to server.js.

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,11 +1,26 @@
+import "dotenv/config";
 import pg from "pg";
 
 const { Pool } = pg;
 
 export const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DG_HOST,
-  database: process.env.DG_NAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+});
+
+export async function testDatabaseConnection() {
+  const result = await pool.query("SELECT NOW()");
+
+  console.log("Database connected:", result.rows[0].now);
+}
+
+console.log({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  passwordType: typeof process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,20 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
+import "dotenv/config";
 import app from "./app.js";
+import { testDatabaseConnection } from "./config/database.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    await testDatabaseConnection();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  }
+}
+
+startServer();
